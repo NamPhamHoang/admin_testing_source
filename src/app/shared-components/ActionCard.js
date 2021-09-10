4571b57d8bf51709fe4e8a1113958553e9c4f93c
Nam Pham <thinhpham@play-force.com.au>
2021-09-10 13:22:04 +0700

    admin demo source

---
+++ src/app/shared-components/ActionCard.js
@@ -0,0 +1,19 @@
+import React, { forwardRef } from "react";
+import * as PropTypes from "prop-types";
+
+const ActionCard = forwardRef((props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+});
+
+ActionCard.propTypes = {
+    header: PropTypes.node,
+    content: PropTypes.node,
+    
+}
+
+ActionCard.defaultProps = {}
+return ActionCard;
